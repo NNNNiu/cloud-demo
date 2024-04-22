66042f767ab6489e39b31dcba275f4473bc36356
NNNNiu <510704071@qq.com>
2024-04-22 14:01:37 +0800

    不同服务之间的调用 Order中的接口调用User中的接口

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,11 +2,15 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private OrderMapper orderMapper;
@@ -14,6 +18,11 @@ public class OrderService {
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.跨服务调用
+        String url = "http://localhost:8081/user/"+order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        // 3.数据封装
+        order.setUser(user);
         // 4.返回
         return order;
     }
